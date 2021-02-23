0b6b057fe00c7720a42decd97cbca37a1e0fee71
bblomme <baptiste.blomme@ig2i.centralelille.fr>
2021-02-23 09:17:11 +0000

    avalam-refresh fonctionnel

--- diag/diag.c
+++ diag/diag.c
@@ -1,18 +1,67 @@
 #include "avalam.h"
 #include "topologie.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
-int main(int argc, char *argv[], char *fen)
+
+int main(int argc, char argv[], char *fen)
 {
-	t=topologie();
+	T_Voisins t;
 	FILE* fichier = NULL;
+	t=topologie[NBCASES];
+	
 	if (argv[0] != '\0')
 	{
-		strcpy(argv,"position_description.json");
+		strcpy(argv,"position_description.json");//création du fichier json
 	}	
   fichier = fopen(argv, "w+");
 
-  FEN(t,argv);
+  fprintf(fichier,"traiterJson({\n%s:%d,\n%s:%d,\n%s:%d,\n%s:%d,\n%s:%d,\n%s:[\n",STR_TURN,p.trait,STR_COLS);
+      for(i=0;i<NBCASES;i++) 
+      {
+		    fprintf(fichier,"{%s:%3d, %s:%3d},\n", STR_NB, p.cols[i].nb,STR_COULEUR, p.cols[i].couleur);
+      }
+      fprintf(fichier,"]\n});");
+
+	fclose(fichier);
+ // fen(t,argv);
 
 }
+
+/*
+char saisie (char tab)
+{
+tab [80];
+int i;
+char fg
+
+printf("\nSaisissez votre chaîne:")
+fgets(fg,80,stdin);
+while(i!=('u,d,t,q,c,U,D,T,Q,C'))
+
+
+
+}
+
+
+//Description compacte d’une position d’échecs
+//rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w
+//avalam-fen : utilisation des symboles 
+//u d t q c pour les jaunes
+//U D T Q C pour les rouges
+//1 … n pour indiquer un nombre de colonnes vides
+//en suivant l’ordre des indices des cases
+//un espace; puis j/r pour le trait
+
+
+
+
+			fichier = fopen(argv, "w+");
+
+			stdout = fichier;
+			afficherPosition(p);
+			afficherScore(s);
+      
+      
--- standalone/standalone.c
+++ standalone/standalone.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "avalam.h"
 #include "topologie.h"
 
 
@@ -10,13 +6,13 @@ int main(int argc, char argv[])
 	FILE* fichier = NULL;
 	if (argv[0] != '\0')
 	{
-		strcpy(argv,"position_score.json");
+		strcpy(argv,"refresh-data.js"); //création fichier js
 	}
 	
 	
 	_Bool FALSE;
 	int pstop = 1, choix, choix2, cancel = 1;
-  int i; //variable comptage
+  	int i; //variable comptage
 	T_Score s; //score
 	T_Position p; //position
 	char af; //????
@@ -39,18 +35,18 @@ int main(int argc, char argv[])
 			printf("Quel pion voulez-vous jouer ?\n");
 			scanf("%d",&choix);
 			printf("Où le déplacer ? Tapez un nombre supérieur à 47 pour annuler\n");
-	    scanf("%d",&choix2);
+	    		scanf("%d",&choix2);
 			if((choix2>47 || choix2<0 || choix<0 ||choix>47) && (estValide(p,choix,choix2))==FALSE) 
-				{
-					cancel=1;
-				}
+			{
+				cancel=1;
+			}
 			else
 			{
 				cancel=0;
 				p=jouerCoup(p,choix,choix2);
 				addCoup(&l,choix,choix2);
           
-      }
+      			}
       
 			s=evaluerScore(p);
 			afficherScore(s);
@@ -62,12 +58,12 @@ int main(int argc, char argv[])
 			afficherPosition(p);
 			afficherScore(s);
       */
-      fprintf(fichier,"traiterJson({\n%s:%d,\n%s:%d,\n%s:%d,\n%s:%d,\n%s:%d,\n%s:[\n",STR_TURN,p.trait,STR_SCORE_J,s.nbJ,STR_SCORE_J5,s.nbJ5,STR_SCORE_R,s.nbR,STR_SCORE_R5,s.nbR5,STR_COLS);
-      for(i=0;i<NBCASES;i++) 
-      {
-		    fprintf(fichier,"{%s:%3d, %s:%3d},\n", STR_NB, p.cols[i].nb,STR_COULEUR, p.cols[i].couleur);
-      }
-      fprintf(fichier,"]\n});");
+			fprintf(fichier,"traiterJson({\n%s:%d,\n%s:%d,\n%s:%d,\n%s:%d,\n%s:%d,\n%s:[\n",STR_TURN,p.trait,STR_SCORE_J,s.nbJ,STR_SCORE_J5,s.nbJ5,STR_SCORE_R,s.nbR,STR_SCORE_R5,s.nbR5,STR_COLS);
+			for(i=0;i<NBCASES;i++) 
+			{
+			fprintf(fichier,"{%s:%3d, %s:%3d},\n", STR_NB, p.cols[i].nb,STR_COULEUR, p.cols[i].couleur);
+			}
+			fprintf(fichier,"]\n});");
 
 			fclose(fichier);
 		}
